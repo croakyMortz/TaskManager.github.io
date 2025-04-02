5bea43e0448579a23af6ba0ae728c4c12b7f02bf
MohYou <168301725+croakyMortz@users.noreply.github.com>
2025-04-02 01:34:28 +0100

    Update priTable.js

--- priTable.js
+++ priTable.js
@@ -1,34 +1,60 @@
  import { getTasks, toDoList } from './tdl.js';
 
-console.log(getTasks());
 
-//proof of receiving data, now to fix priority or create code for priority arrangement.
+/* 5th Mar: 
+   Movement of high priority achieved with use of eventlistner listening for changes 
+   a tasks priority, chnaging priority will move task to 'high' table atm, delete 
+   button has also been added with task to remove when the user chooses to though
+   the task itself will remain where the inputt of tasks is, 
+   
+   someway to make that disappear after user chooses a priority?
 
-export const displayToDo = () => {
-    getTasks().forEach(toDo => {
-        console.log(toDo);
-    });
+   
+*/
+let tasks = getTasks();
+
+
+
+
+
+const stor = document.getElementById("stor");
+
+const priHi = document.getElementById("priHi");
+const priMe = document.getElementById("priMe");
+const priLo = document.getElementById("priLo");
+
+const inpButt = document.getElementById("addATaskmM");
+
+stor.addEventListener("change", () => {
+    const newDiv = document.createElement("div");
+    const deleteButton = document.createElement("button"); 
+    deleteButton.textContent = ('Del');
+    deleteButton.onclick = function () {
+    newDiv.remove();
 };
 
-displayToDo();
 
-/* const tas = getTasks();
-console.log(tas);
+    if (String(tasks).match("Priority: High")) {
+        newDiv.append(tasks);
+        newDiv.append(deleteButton);
+        priHi.append(newDiv);
+    }
 
+    if (String(tasks).match("Priority: Medium")) {
+        newDiv.append(tasks);
+        newDiv.append(deleteButton);
+        priMe.append(newDiv);
+    }
 
-
-
-import { mn } from './tdl.js';
-
-const pri = () => {
-    const value = document.getElementById("stor").value;
-    const chosen = document.createTextNode(value);
+    if (String(tasks).match("Priority: Low")) {
+        newDiv.append(tasks);
+        newDiv.append(deleteButton);
+        priLo.append(newDiv);
+    }
+    tasks.pop()
     
-
-};
-
+});
 
 
-console.log(mn()); */
 
 
